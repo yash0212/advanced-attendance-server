6a0f3292ab5497e52a0b832ea9a56f1b156d1381
Yash Lotan <yashlotan7@gmail.com>
2020-01-12 14:30:38 +0530

    Basic login and register Implemented

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+            'regno' => 'required|max:255'
+        ]);
+        $validatedData["password"] = bcrypt($validatedData["password"]);
+        $user = User::create($validatedData);
+
+        $access_token = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $access_token]);
+    }
+
+    public function login(Request $request)
+    {
+        $login_data = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($login_data)){
+            return response(['message'=>'Invalid Credentials']);
+        }
+        $user = auth()->user();
+        $access_token = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'access_token' => $access_token]);
+    }
+}
